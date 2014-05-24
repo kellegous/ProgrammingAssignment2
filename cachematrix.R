64dbe9ef00232283a46110dc34d26063448fa970
Kelly  Norton <knorton@etsy.com>
2014-05-24 15:50:36 -0400

    Adds cache matrix functionality.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functionality for a matrix object that is capable of caching
+## the results of expensive calculations such that repeated calls
+## to expensive operations (like taking the inverse) are faster.
 
-## Write a short comment describing this function
-
+## Create a new matrix that is capable of caching results
+## for the inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+	# A list that holds cached objects
+	cache <- list()
+	list(
+		set = function(y) {
+			x <<- y
+			cache <<- list()
+		},
+		get = function() x,
+		solve = function(...) {
+			ans <- cache$solve
+			if (is.null(ans)) {
+				ans <- solve(x, ...)
+				cache$solve <<- ans
+			}
+			ans
+		})
 }
 
 
-## Write a short comment describing this function
+## Inverts the matrix represented by x. This function will
+## use an internally cached value if the inverse was previously
+## computed.
+cacheSolve <- function(x, ...) x$solve(...)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
